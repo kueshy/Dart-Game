999803d1ea5c0b282212683991f703f7eaa34243
kueshy <eliasemonyi@yahoo.com>
2023-04-16 14:47:48 +0300

    Dart Game first commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner keyboard=new Scanner(System.in);
+
+        System.out.println("Enter the first coordinate");
+        int x=keyboard.nextInt();
+
+
+        System.out.println("Enter the second coordinate");
+        int y=keyboard.nextInt();
+
+        System.out.println(score(x,y));
+    }
+
+    public static String score(int x, int y){
+        double distance_to_origin = Math.sqrt(x*x + y*y);
+
+        if(distance_to_origin > 10){
+            return "Score is "+ 0;
+        }else if (distance_to_origin > 5){
+            return "Score is "+ 1;
+        }else if(distance_to_origin > 1){
+            return "Score is "+ 5;
+        }else {
+            return "Score is "+ 10;
+        }
+    }
+}
